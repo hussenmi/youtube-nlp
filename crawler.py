b128e59abe9b0af6709713f018d901522f3aeea8
christopherfkk <christopherfkk@gmail.com>
2022-02-08 17:33:22 +0100

    editted

--- crawler.py
+++ crawler.py
@@ -126,34 +126,6 @@ class ApiCrawler():
 # comments = crawler.get_comments("___NoMi5pp0") # randome Korean video with 141 comments + replies
 # print(comments)
 
-import time
-  
-# Function that computes Fibonacci numbers without lru_cache
-def fib_without_cache(n):
-    if n < 2:
-        return n
-    return fib_without_cache(n-1) + fib_without_cache(n-2)
-      
-begin = time.time()
-fib_without_cache(50)
-end = time.time()
-
-print("Time taken to execute the function without lru_cache is", end-begin)
-  
-# Function that computes Fibonacci
-# numbers with lru_cache
-@lru_cache
-def fib_with_cache(n):
-    if n < 2:
-        return n
-    return fib_with_cache(n-1) + fib_with_cache(n-2)
-      
-begin = time.time()
-fib_with_cache(50)
-end = time.time()
-  
-print("Time taken to execute the \function with lru_cache is", end-begin)
-
 '''
 Sample commentThreads.list() JSON response
 {
@@ -165,7 +137,7 @@ Sample commentThreads.list() JSON response
     "resultsPerPage": integer
   },
   "items": [
-    commentThread Resource
+    commentThread Resource ***
   ]
 }
 
@@ -184,7 +156,7 @@ Sample commentThread Resource object:
   },
   "replies": {
     "comments": [
-      comments Resource
+      comments Resource ****
     ]
   }
 }
