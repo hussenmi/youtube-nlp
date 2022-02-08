4dd5ce15fa5dd39ff27214920da51f42292e82fc
christopherfkk <christopherfkk@gmail.com>
2022-02-08 20:34:35 +0100

    added DBconnector

--- database.py
+++ database.py
@@ -1,41 +1,48 @@
-# medium https://towardsdatascience.com/a-practical-guide-to-getting-set-up-with-postgresql-a1bf37a0cfd7
+'''
+Great medium article to set up PostgreSQL and pgAdmin (GUI for PostgreSQL)
+https://towardsdatascience.com/a-practical-guide-to-getting-set-up-with-postgresql-a1bf37a0cfd7
 
-# download postgresql and pgAdmin 4
+Requirements:
+- download postgreSQL and pgAdmin 4 https://www.postgresql.org/download/ 
+- pip install SQLAlchemy
+- pip install pandas
 
-# pip install SQLAlchemy
-
-# Connecting to Postgres default username: postgres; localhost:5432
-from sqlalchemy import create_engine
-connection_uri = 'postgres://username:password@host:port/database'
-engine = create_engine(connection_uri)
-
-# Executing SQL command
-with engine.connect() as con:
-    con.execute(sql)
-
+'''
 from sqlalchemy import create_engine
 import os
 
-# Connecting to Postgres
-connection_uri = 'postgresql://postgres:test@localhost:5432/covid-19'
-engine = create_engine(connection_uri)
+class DBconnector():
 
-# Reading our SQL script
-script_name = 'create_table.sql'
-dirname = os.path.dirname(__file__)
-sql_file = os.path.join(dirname, script_name)
+    def __init__(self, username="postgres", password="youtubenlp", host="localhost", port="5432") -> None:
+        self.username = username
+        self.password = password
+        self.host = host
+        self.port = port
+        self.con_url = "postgres://username:password@host:port/database"
+        self.engine = create_engine(self.con_url)
 
-sql = open(sql_file, 'r').read()
+    def execute_sql_query(self, sql) -> None:
+        # input: SQL command as a string
+        with self.engine.connect() as con:
+            con.execute(sql)
 
-try:
-    # Executing SQL commands
-    with engine.connect() as con:
-        con.execute(sql)
-    print(f'Successfully executed {script_name}')
+    def execute_sql_from_file(self, sql_file) -> None:
+        # read SQL script
+        script_name = sql_file
+        dirname = os.path.dirname(__file__)
+        sql_file = os.path.join(dirname, script_name)
 
-except Exception as err:
-    print(f'Failed to execute {script_name}')
-    print(f'Error:\n{err}')
+        sql = open(sql_file, 'r').read()
+
+        try:
+            # Executing SQL commands
+            with self.engine.connect() as con:
+                con.execute(sql)
+            print(f'Successfully executed {script_name}')
+
+        except Exception as err:
+            print(f'Failed to execute {script_name}')
+            print(f'Error:\n{err}')
 
 
 # Load DataFrame into database: SQLAlchemy X Pandas
